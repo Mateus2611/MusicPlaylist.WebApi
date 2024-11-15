45a37a244940df80a4e484b1534f1d8ef541731b
Mateus <MateusHAS.dev@gmail.com>
2024-11-15 16:58:27 -0300

    deploy swagger to azure

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>( options =>
+builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
         .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
         .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
 });
 
-builder.Services.AddSwaggerGen( options =>
+builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
@@ -43,14 +43,12 @@ builder.Services.AddScoped<IMusicsPlaylists, EFMusicsPlaylists>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(opttions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(opttions =>
-    {
-        opttions.SwaggerEndpoint("/swagger/v1/swagger.json", "MusicPlaylist.WebApi v1");
-    });
-}
+    opttions.SwaggerEndpoint("/swagger/v1/swagger.json", "MusicPlaylist.WebApi v1");
+});
+
 
 app.Use(async (context, next) =>
 {
